500713655f5f58a6a6eff6ca58be17d49a3bfdda
AgraniJain <agrani.jain2023@vitstudent.ac.in>
2024-09-09 16:26:32 +0000

    container config done

---
+++ 1_bas/bas/var.js
@@ -0,0 +1,7 @@
+const accountId=123
+let accountEmail="agrani.com"
+var accountPassword="123456"
+accountCity="Bhopal"
+// accountId=1523 // cannot change
+console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,accountCity])
